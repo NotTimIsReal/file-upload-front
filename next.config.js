7a73350dffcef3dd02125345fa99f1544768c297
NotTimIsReal <alastairrintumathew@gmail.com>
2022-02-28 19:01:46 +1100

    brought to base

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    API: process.env.API,
+    API: "http://localhost:9090",
   },
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-import * as axios from "axios";
+import axios from "axios";
 import type { NextPage } from "next";
-import { Box } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
-import Navbar from "../components/navbar";
-import Features from "../components/features";
+import formdata from "form-data";
 const Home: NextPage = () => {
   type User = {
     userid: string;
@@ -13,87 +11,38 @@ const Home: NextPage = () => {
   };
   const [loggedin, setLoggedin] = useState<boolean>(false);
   const [user, setUser] = useState<User | null>();
-  const getUser = async () => {
-    await axios.default
-      .get(process.env.API + "account/user", { withCredentials: true })
-      .then((res) => {
-        if (res.data === 405) {
-          setLoggedin(false);
-        } else if (typeof res.data === "object") {
-          setLoggedin(true);
-          setUser(res.data);
-        }
-      })
-      .catch((err) => {
-        null;
-      });
+  const sendFile = async (data: any) => {
+    await axios.post(`${process.env.API}/account/898920274945/newfile`, data, {
+      withCredentials: true,
+    });
+  };
+  const signIN = async () => {
+    await axios.post(
+      `${process.env.API}/auth/login`,
+      {
+        id: "898920274945",
+        password: "password",
+      },
+      { withCredentials: true }
+    );
   };
   useEffect(() => {
-    getUser();
-  });
+    signIN();
+  }, []);
   return (
     <>
-      <Navbar loggedin={loggedin} user={user} />
-      <Box border="black" padding="0" className="login-box spacer">
-        <h1
-          style={{
-            position: "relative",
-            textAlign: "center",
-            top: "25%",
-            fontFamily: "roboto, sans-serif",
-            fontSize: "5rem",
-            fontWeight: "bolder",
-          }}
-        >
-          File Hosting
-        </h1>
-      </Box>
-      <Box
-        width="100%"
-        position="relative"
-        overflow="hidden"
-        marginRight="1em"
-        textAlign="center"
-      >
-        <h1
-          style={{
-            position: "relative",
-            fontSize: "40px",
-            fontFamily: "Manrope",
-          }}
-        >
-          Why Choose Us?
-        </h1>
-        <ul style={{ display: "inline-block" }}>
-          <Features text="Free And Open Source" />
-          <Features text="Easy To Edit" />
-          <Features text="Available on github" />
-          <Features text="Has an open-source api" />
-        </ul>
-      </Box>
-      <Box
-        width="100%"
-        position="relative"
-        overflow="hidden"
-        marginRight="1em"
-        textAlign="center"
-      >
-        <h1
-          style={{
-            position: "relative",
-            fontSize: "40px",
-            fontFamily: "Manrope",
-          }}
-        >
-          FAQS
-        </h1>
-        <ul style={{ display: "inline-block" }}>
-          <dd> How Do I upload?</dd>
-          <Features text="You Login First and then go to the dashboard"></Features>
-          <dd>Can I use this website for my own frontend?</dd>
-          <Features text="Yes feel free you just need your api to fit with this and edit the .env files" />
-        </ul>
-      </Box>
+      <input
+        type={"file"}
+        multiple
+        onChange={(data) => {
+          const d = new formdata();
+          if (data.target.files)
+            for (let i = 0; i < data.target.files?.length; i++) {
+              d.append("file", data.target.files[i]);
+            }
+          sendFile(d);
+        }}
+      ></input>
     </>
   );
 };
